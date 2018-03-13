823877686c2a4fb78774e163932331334e4362fb
Kevin Hering <chumlabs@gmail.com>
2018-03-13 09:56:23 -0400

    remove test data in app.js & tweak .gitignore

--- src/app.js
+++ src/app.js
@@ -15,42 +15,6 @@ import getVisibleExpenses from './selectors/expenses';
 
 const store = configureStore();
 
-// tests
-const billOne = store.dispatch(
-  addExpense({
-    description: 'duck bill',
-    note: 'standard duck bill - Mallard',
-    amount: 3495,
-    createdAt: 1518886800000
-  })
-);
-
-const billTwo = store.dispatch(
-  addExpense({
-    description: 'platypus bill',
-    note: 'very rare bill',
-    amount: 110000,
-    createdAt: 1520010000000
-  })
-);
-
-const billThree = store.dispatch(
-  addExpense({
-    description: 'pecker bill',
-    note: "that pecker won't be bothering us again",
-    amount: 1200,
-    createdAt: 1520362972316
-  })
-);
-
-const unsubscribe = store.subscribe(() => {
-  const state = store.getState();
-  const visibleExpenses = getVisibleExpenses(state.expenses, state.filters);
-  console.log(visibleExpenses);
-});
-
-unsubscribe();
-
 const mainApp = (
   <Provider store={store}>
     <AppRouter />
